e99fbc5dcb21dc49a9c6c7449b5873b90d4d0546
tmwork1 <tmtm.holmes@gmail.com>
2026-01-28 23:29:55 +0900

    ハンドラ関数の戻り値を HandlerReturn 型に統一し、PP 消費処理を追加

--- src/jpoke/handlers/move.py
+++ src/jpoke/handlers/move.py
@@ -2,40 +2,32 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any
 if TYPE_CHECKING:
     from jpoke.core import Battle
-    from jpoke.model import Pokemon, Move
 
 from jpoke.utils.enums import Interrupt
 from jpoke.core.event import Event, EventContext, HandlerReturn
-from . import common
 
 
-def pivot(battle: Battle, ctx: EventContext, value: Any):
-    player = battle.find_player(ctx.attacker)
-    if battle.get_available_switch_commands(player):
-        player.interrupt = Interrupt.PIVOT
-    return HandlerReturn(True)
-
-
-def blow(battle: Battle, ctx: EventContext, value: Any):
-    player = battle.find_player(ctx.defender)
-    commands = battle.get_available_switch_commands(player)
-    if commands:
-        command = battle.random.choice(commands)
-        battle.run_switch(player, player.team[command.idx])
-    return HandlerReturn(True)
-
-# 共通ハンドラ
-
-
-def reveal_move(battle: Battle, ctx: EventContext, value: Any):
-    ctx.move.revealed = True
-    battle.add_turn_log(ctx.attacker, ctx.move.name)
-    return HandlerReturn(True)
-
-
-def consume_pp(battle: Battle, ctx: EventContext, value: Any):
+def consume_pp(battle: Battle, ctx: EventContext, value: Any) -> HandlerReturn:
     v = battle.events.emit(Event.ON_CHECK_PP_CONSUMED, ctx, 1)
     ctx.move.pp = max(0, ctx.move.pp - v)
     battle.add_turn_log(ctx.attacker, f"PP -{v}")
     ctx.attacker.pp_consumed_moves.append(ctx.move)
     return HandlerReturn(True)
+
+
+def pivot(battle: Battle, ctx: EventContext, value: Any) -> HandlerReturn:
+    player = battle.find_player(ctx.attacker)
+    success = bool(battle.get_available_switch_commands(player))
+    if success:
+        player.interrupt = Interrupt.PIVOT
+    return HandlerReturn(success)
+
+
+def blow(battle: Battle, ctx: EventContext, value: Any) -> HandlerReturn:
+    player = battle.find_player(ctx.defender)
+    commands = battle.get_available_switch_commands(player)
+    success = bool(commands)
+    if success:
+        command = battle.random.choice(commands)
+        battle.run_switch(player, player.team[command.idx])
+    return HandlerReturn(success)
