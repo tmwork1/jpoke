142ae2f501f9f644dde77dbc66bf58585c86bed0
tmwork1 <tmtm.holmes@gmail.com>
2026-07-09 07:22:54 +0900

    chore: バッチ整形・テスト一覧再生成

--- tests/moves_status/test_move__grouped.py
+++ tests/moves_status/test_move__grouped.py
@@ -43,21 +43,6 @@ def test_フィールド展開技_すでに同じフィールドなら失敗(ter
     assert battle.terrain.count == 5
 
 
-@pytest.mark.parametrize("move_name,blocking_weather", [
-    ("にほんばれ", "おおあめ"),    # にほんばれはおおあめ中失敗
-    ("あまごい", "おおひでり"),    # あまごいはおおひでり中失敗
-])
-def test_天候始動技_強天候で失敗(move_name, blocking_weather):
-    """天候始動技: 反対の強い天候が有効なときは失敗する"""
-    battle = t.start_battle(
-        team0=[Pokemon("ピカチュウ", move_names=[move_name])],
-        team1=[Pokemon("カビゴン")],
-        weather=(blocking_weather, 99),
-    )
-    t.run_move(battle, 0)
-    assert battle.weather.name == blocking_weather
-
-
 @pytest.mark.parametrize("weather_name,move_name", [
     ("はれ", "にほんばれ"),
     ("あめ", "あまごい"),
@@ -74,3 +59,18 @@ def test_天候始動技_すでに同じ天候なら失敗(weather_name, move_na
     # カウントは変わらない（再発動されない）
     assert battle.weather.name == weather_name
     assert battle.weather.count == 5
+
+
+@pytest.mark.parametrize("move_name,blocking_weather", [
+    ("にほんばれ", "おおあめ"),    # にほんばれはおおあめ中失敗
+    ("あまごい", "おおひでり"),    # あまごいはおおひでり中失敗
+])
+def test_天候始動技_強天候で失敗(move_name, blocking_weather):
+    """天候始動技: 反対の強い天候が有効なときは失敗する"""
+    battle = t.start_battle(
+        team0=[Pokemon("ピカチュウ", move_names=[move_name])],
+        team1=[Pokemon("カビゴン")],
+        weather=(blocking_weather, 99),
+    )
+    t.run_move(battle, 0)
+    assert battle.weather.name == blocking_weather
