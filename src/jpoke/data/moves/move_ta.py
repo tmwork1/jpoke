daebacbd0469e16dfdb210459d453a4a6d4d669d
tmwork1 <tmtm.holmes@gmail.com>
2026-07-09 12:45:01 +0900

    review: とおぼえ

--- src/jpoke/data/moves/move_ta.py
+++ src/jpoke/data/moves/move_ta.py
@@ -920,7 +920,8 @@ MOVES_TA: dict[MoveName, MoveData] = {
     "とおぼえ": MoveData(
         type="ノーマル",
         category="status",
-        pp=40,
+        pp=20,
+        target="self",
         flags={"sound"},
         handlers={
             Event.ON_STATUS_HIT: h.MoveHandler(
